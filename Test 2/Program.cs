a0e025f62706a909eeb88f1d7c1e12dd67e3d30f
mopa01 <111962656+Dungeater1337@users.noreply.github.com>
2023-02-17 10:53:51 +0100

    Add project files.

---
+++ Test 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System; 
+namespace Test._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilken av följande alternativ vill du omvandla? Skriv 1 elle 2:");
+            Console.WriteLine("1. Omvandla från Celsius till Fahrenheit");
+            Console.WriteLine("2. Omvandla från Fahrenheit till Celsius");
+            string användarensval = Console.ReadLine();
+
+
+            switch (användarensval)
+            {
+                case "1":
+                    Celsius();
+                    Console.WriteLine();
+                    break;
+
+                case "2":
+                    Fahrenheit();
+                    Console.WriteLine();
+                    break;
+            }
+
+        }
+
+        static void Celsius()
+        {
+            int tal1 = 32;
+            double tal2 = 1.8;
+            Console.WriteLine("Skriv in tal för att omvandla Celsius till Fahrenheit: ");
+            int tal = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Svaret till tal 1 är:{tal1 + (tal * tal2)}");
+            
+        }
+
+        static void Fahrenheit()
+        {
+            int tal3 = 32;
+            double tal4 = 1.8;
+            Console.WriteLine("Skriv in tal för att omvandla Fahrenheit till Celsius: ");
+            int tal = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Svaret till tal 2 är:{(tal3 - tal) / tal4}");
+        }
+    }
+}
